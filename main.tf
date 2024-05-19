984903b147dd1d359ec7222079e4a110cb1be74c
petergillhub <petergillhmg@gmail.com>
2024-05-19 23:45:31 +0100

    first commit

---
+++ main.tf
@@ -0,0 +1,27 @@
+terraform {
+  required_version = "~>1.6.0"
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "~>5.26.0"
+    }
+  }
+
+}
+
+provider "aws" {
+    region = "eu-west-2"
+  
+}
+
+resource "aws_instance" "name" {
+
+    ami = "ami-035cecbff25e0d91e"
+    instance_type = "t2.micro"
+    availability_zone = "eu-west-2a"
+    key_name = "subina.pem"
+    tags = {
+      "Name" = "DevOPs Instance"
+    }
+  
+}
