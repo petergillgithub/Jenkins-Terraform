8e031252f6008f6c9abb4cede22c79e14ce02030
petergillhub <petergillhmg@gmail.com>
2024-05-20 12:29:06 +0100

    update jenkinsfile and main

--- main.tf
+++ main.tf
@@ -19,7 +19,7 @@ resource "aws_instance" "name" {
     ami = "ami-035cecbff25e0d91e"
     instance_type = "t2.micro"
     availability_zone = "eu-west-2a"
-    key_name = "subina.pem"
+    key_name = "subina"
     tags = {
       "Name" = "DevOPs Instance"
     }
